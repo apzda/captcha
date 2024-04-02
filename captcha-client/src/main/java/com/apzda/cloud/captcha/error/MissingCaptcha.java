5f8bc33968f1922521040591a5ff1c38fce3ea63
ninggf <windywany@gmail.com>
2024-04-02 15:58:33 +0800

    添加缺失验证码与需要提供验证错误

--- captcha-client/src/main/java/com/apzda/cloud/captcha/error/MissingCaptcha.java
+++ captcha-client/src/main/java/com/apzda/cloud/captcha/error/MissingCaptcha.java
@@ -2,7 +2,12 @@ package com.apzda.cloud.captcha.error;
 
 import com.apzda.cloud.gsvc.IServiceError;
 
-public record MissingCaptcha(String message) implements IServiceError {
+public record MissingCaptcha() implements IServiceError {
+    @Override
+    public String message() {
+        return "Captcha is missing";
+    }
+
     @Override
     public int code() {
         return 3;
--- captcha-client/src/main/java/com/apzda/cloud/captcha/error/NeedCaptcha.java
+++ captcha-client/src/main/java/com/apzda/cloud/captcha/error/NeedCaptcha.java
@@ -2,7 +2,12 @@ package com.apzda.cloud.captcha.error;
 
 import com.apzda.cloud.gsvc.IServiceError;
 
-public record NeedCaptcha(String message) implements IServiceError {
+public record NeedCaptcha() implements IServiceError {
+    @Override
+    public String message() {
+        return "Captcha is needed";
+    }
+
     @Override
     public int code() {
         return 4;
